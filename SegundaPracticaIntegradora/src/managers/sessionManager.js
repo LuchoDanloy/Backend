87329d70a9f649193a02501b710094379afd3511
Lucho Danloy <lucianodanloy@gmail.com>
2023-06-06 19:39:15 -0300

    Segunda Practica Integradora

---
+++ SegundaPracticaIntegradora/src/managers/sessionManager.js
@@ -0,0 +1,42 @@
+import UserManager from "../managers/userManager.js";
+import { isValidPassword, generateToken, createHash } from "../utils/index.js";
+import loginValidation from "../validations/session/loginValidation.js";
+import userCreateValidation from "../validations/user/userCreateValidation.js";
+
+class sessionManager{
+
+    async login(email, password)
+    {
+        await loginValidation.parseAsync({ email, password });
+
+        const manager = new UserManager();
+        const user = await manager.getOneByEmail(email);
+        const isHashedPassword = await isValidPassword(password, user.password);
+
+        if (!isHashedPassword)
+        {
+            return res.status(401).send({ message: 'Login failed, invalid password.'})
+        }
+
+        return await generateToken(user);
+    }
+
+    async signup(payload)
+    {
+      await userCreateValidation.parseAsync(payload);
+  
+      const dto = {
+        ...payload,
+        password: await createHash(payload.password, 10)
+      }
+  
+      const manager = new UserManager();
+      const user = await manager.create(dto);
+      //const user  = await this.userDao.create(dto);
+  
+      return { ...user, password: undefined};
+    }
+}
+
+
+export default sessionManager;
