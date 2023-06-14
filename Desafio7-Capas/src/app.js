cfe2b807739b11164adea979aff224e6d4ec02b3
Lucho Danloy <lucianodanloy@gmail.com>
2023-06-13 21:18:09 -0300

    Desafio 7 division capas data, domine, presentation

---
+++ Desafio7-Capas/src/app.js
@@ -0,0 +1,24 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import mongoose from 'mongoose';
+
+import AppFactory from "./presentation/factories/appFactory.js";
+
+void (async() =>
+{
+
+    //CONEXION CON MONGO-ATLAS
+    await mongoose.connect(process.env.MONGO_DB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      });
+
+      const app = AppFactory.create();
+
+      app.init();
+      app.build();
+      app.listen();
+
+})();
+
---
+++ Desafio7-Capas/src/domine/managers/ProductManager.js
@@ -0,0 +1,75 @@
+import productMongooseDao from "../../data/dao/productMongooseDao.js"
+
+class ProductManager{
+
+    constructor(){
+        this.dao = new productMongooseDao();
+    }
+
+    async getProducts(limit, query, sort){
+        try{
+            //LIMIT
+            if (!limit){
+                limit=10
+            }
+            //SORT
+            if (!sort){
+                sort = 1
+            }else{
+                if(sort=="asc" || sort=="desc"){
+                    sort = sort == "asc" ? 1 : -1
+                }else{
+                    sort = 1
+                }
+            }
+
+            return this.dao.getProducts(limit, query, sort);    //van sin await en caso de retornar directamente porque ya va a estar el await en el Controller cuando sea invocado.
+        }
+        catch(error){
+            console.error(error);
+        }
+    }
+
+    async addProduct(product){
+
+        try{
+            return this.dao.addProduct(product);
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
+
+    async getProductById(pid){
+
+        try{
+            return this.dao.getProductById(pid);
+        }
+        catch(error){
+            console.error(error);
+        }
+
+    }
+
+    async updateProduct(pid, newProduct){
+        
+        try{
+            return this.dao.updateProduct(pid, newProduct);
+        }
+        catch(error){
+            console.error(error);
+        }
+    }
+
+
+    async deleteProduct(pid){
+        try{
+            return this.dao.deleteProduct(pid);
+        }
+        catch(error){
+            console.error(error);
+        }       
+    }
+}
+
+export default ProductManager;
