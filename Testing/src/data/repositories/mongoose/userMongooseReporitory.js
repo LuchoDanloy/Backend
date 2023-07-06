f2afbc5422a89f811af6fd7148ce75b221417242
Lucho Danloy <lucianodanloy@gmail.com>
2023-07-06 18:39:03 -0300

    Testing

---
+++ Testing/src/data/repositories/mongoose/userMongooseReporitory.js
@@ -0,0 +1,102 @@
+import userSchema from "../../models/mongoose/userSchema.js";
+import User from '../../../domine/entities/user.js';
+
+class UserMongooseReporitory
+{
+  async paginate(criteria)
+  {
+    const { limit, page } = criteria;
+    const userDocuments = await userSchema.paginate({}, { limit, page });
+    const { docs, ...pagination } = userDocuments;
+
+    const users = docs.map(document => new User({
+      id: document._id,
+      firstName: document.firstName,
+      lastName: document.lastName,
+      email: document.email,
+      age: document.age,
+    }));
+
+    return {
+      users,
+      pagination
+    };
+  }
+
+  async getOne(id)
+  {
+    const userDocument = await userSchema.findOne({ _id: id });
+
+    if(!userDocument)
+    {
+      throw new Error('User dont exist.');
+    }
+
+    return new User({
+        id: userDocument?._id,
+        firstName: userDocument?.firstName,
+        lastName: userDocument?.lastName,
+        email: userDocument?.email,
+        age: userDocument?.age,
+        password: userDocument?.password
+    });
+  }
+
+  async getOneByEmail(email)
+  {
+    const userDocument = await userSchema.findOne({ email });
+
+    if(!userDocument)
+    {
+      throw new Error('User dont exist.');
+    }
+
+    return new User({
+        id: userDocument?._id,
+        firstName: userDocument?.firstName,
+        lastName: userDocument?.lastName,
+        email: userDocument?.email,
+        age: userDocument?.age,
+        password: userDocument?.password
+    });
+  }
+
+  async create(data)
+  {
+    const userDocument = await userSchema.create(data);
+
+    return new User({
+        id: userDocument._id,
+        firstName: userDocument.firstName,
+        lastName: userDocument.lastName,
+        email: userDocument.email,
+        age: userDocument.age,
+        password: userDocument.password,
+    });
+  }
+
+  async updateOne(id, data)
+  {
+    const userDocument = await userSchema.findOneAndUpdate({ _id: id }, data, { new: true});
+
+    if(!userDocument)
+    {
+      throw new Error('User dont exist.');
+    }
+
+    return new User({
+        id: userDocument._id,
+        firstName: userDocument.firstName,
+        lastName: userDocument.lastName,
+        email: userDocument.email,
+        age: userDocument.age
+    });
+  }
+
+  async deleteOne(id)
+  {
+    return userSchema.deleteOne({ _id: id });
+  }
+}
+
+export default UserMongooseReporitory;
---
+++ Testing/src/domine/entities/user.js
@@ -0,0 +1,14 @@
+
+class User
+{
+  constructor(props)
+  {
+      this.id = props.id;
+      this.firstName = props.firstName;
+      this.lastName = props.lastName;
+      this.email = props.email;
+      this.age = props.age;
+  }
+}
+
+export default User;
