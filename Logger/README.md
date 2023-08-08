63fe6a8273cc7653bf4c748c663c1261248e3b4b
Lucho Danloy <lucianodanloy@gmail.com>
2023-08-08 07:40:44 -0300

    Logger

---
+++ Logger/README.md
@@ -0,0 +1,6 @@
+
+# Comandos de inicio
+
+```shell
+npm run command -- addUser -fn admin -ln admin -e admin@admin.com -a 32 -p 12345678 -ia true
+```
---
+++ Logger/src/data/repositories/mongoose/cartMongooseRepository.js
@@ -0,0 +1,61 @@
+import cartSchema from  "../../models/mongoose/cartSchema.js"
+
+class cartMongooseRepository{
+
+    async getCarts(){
+        try{    
+            const cartsDocument = await cartSchema.find();   
+            return cartsDocument.map(document=>({
+                id: document._id,
+                products: document.products            
+            })) //como quier devolver un objeto lo tengo que poner entre parentesis (). Si dejo las llaves {} solas interpreta que esta abriendo una funcion.
+        }
+        catch(error){
+            console.error(error);
+        }
+    }
+
+    async getCartById(cid){
+
+        try{
+            const cartDocument = await cartSchema.findOne({_id: cid});
+
+            
+            return {
+                id: cartDocument._id,
+                products: cartDocument.products
+            } 
+        }
+        catch(error){
+            console.error(error);
+        }
+
+    }
+
+    async addCart(cart){
+        try{  
+            const cartDocument = await cartSchema.create(cart);
+
+            return {
+                id: cartDocument._id,
+                products: cartDocument.products
+            } 
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
+
+    async updateCart(idCart, newCart){
+
+        try{
+            return cartSchema.updateOne({_id: idCart}, newCart);
+        }
+        catch(error){
+            console.error(error);
+        }
+    }
+    
+}
+
+export default cartMongooseRepository;
